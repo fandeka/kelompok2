52a4184f20aea11ffa113118144df386ba8c10dc
fandeka <fandeka.aghfirlana@gmail.com>
2018-10-21 16:55:03 +0700

    lepas filter intruktur

--- application/views/school/dashboard.php
+++ application/views/school/dashboard.php
@@ -72,8 +72,8 @@
                 <div class="inner">
                   <h3>
                   <?php 
-                  //$q = $this->db->query("select * from teacher_detail where school_id="._get_current_user_id($this));
-                  $q = $this->db->query("select * from teacher_detail");
+                  $q = $this->db->query("select * from teacher_detail where school_id="._get_current_user_id($this));
+                 // $q = $this->db->query("select * from teacher_detail");
                                     $student =  $q->result();
                                     echo count($student);
                   ?>
